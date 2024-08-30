29d150c8a65b7dd4fa8ff6f2edca3273fa953b2b
speters9 <speters9@stanford.edu>
2024-08-29 20:20:13 -0600

    prep for compile

--- gui/jobmatch_app.py
+++ gui/jobmatch_app.py
@@ -3,6 +3,9 @@ Run the app
 """
 
 import logging
+import sys
+
+from PyQt5.QtWidgets import QApplication
 
 from gui.gui_interface import JobMatchApp
 
@@ -16,11 +19,20 @@ def main():
                         ])
     logging.debug("Starting application...")
     try:
-        app = JobMatchApp()
+        # Ensure QApplication is initialized here
+        app = QApplication(sys.argv)
+
+        # Create the main window
+        window = JobMatchApp()
+        window.show()
+
         logging.debug("App initialized, entering main loop...")
-        app.mainloop()
+
+        # Execute the application
+        app.exec_()
     except Exception as e:
         logging.error("An error occurred", exc_info=True)
+        sys.exit(1)  # Exit with an error code
 
 
 if __name__ == "__main__":
